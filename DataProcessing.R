be2836c247cd4bdb8e49f5d87bdbf1f8868220d4
Nicholas Dell'Omo <dellomo_nicholas@bah.com>
2015-03-26 21:10:47 -0400

    Added more

--- DataProcessing.R
+++ DataProcessing.R
@@ -1,26 +1,36 @@
 setwd("~/Documents/Coursera/Data_Science_Capstone")
-install.packages("tm")
+#install.packages("tm")
+#install.packages("stylo")
+#install.packages("SnowballC")
 library(tm)
+library(stylo)
+library(SnowballC)
 
 blog <- readLines("Data/en_US.blogs.txt")
-blog.s <- blog[ifelse(rbinom(length(blog),1,.05),T,F)]
+blog.s <- blog[ifelse(rbinom(length(blog),1,.02),T,F)]
 rm(blog);gc()
 
 news <- readLines("Data/en_US.news.txt")
-news.s <- news[ifelse(rbinom(length(news),1,.05),T,F)]
+news.s <- news[ifelse(rbinom(length(news),1,.02),T,F)]
 rm(news);gc()
 
 twitter <- readLines("Data/en_US.twitter.txt")
-twitter.s <- twitter[ifelse(rbinom(length(twitter),1,.05),T,F)]
+twitter.s <- twitter[ifelse(rbinom(length(twitter),1,.02),T,F)]
 rm(twitter);gc()
 
+text <- c(blog.s,news.s,twitter.s); rm(blog.s,news.s,twitter.s);gc()
 
-blog.v <- VectorSource(blog.s);rm(blog.s);gc()
-news.v <- VectorSource(news.s);rm(news.s);gc()
-twitter.v <- VectorSource(twitter.s);rm(twitter.s);gc()
+text.v <- VectorSource(text);rm(text);gc()
 
-blog <- Corpus(blog.v);rm(blog.v);gc()
-news <- Corpus(news.v);rm(news.v);gc()
-twitter <- Corpus(twitter.v);rm(twitter.v);gc()
+text.c <- Corpus(text.v);rm(text.v);gc()
 
-blog <- tm_map
+text.c <- tm_map(text.c, tolower)
+text.c <- tm_map(text.c, removePunctuation)
+text.c <- tm_map(text.c, removeNumbers)
+text.c <- tm_map(text.c, removeWords,stopwords("english"))
+text.c <- tm_map(text.c, stemDocument)
+inspect(text.c)
+
+two.gram <- make.ngrams(text.c, ngram.size = 2)
+
+two.gram <- as.data.frame(two.gram)
